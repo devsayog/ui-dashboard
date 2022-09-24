168af0a56bba98952463bb62ded6d0c4a5c9f3d5
devsayog <devsayog@gmail.com>
2022-09-24 10:49:00 +0545

    reafactor nav links to different file

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,66 +1,11 @@
-import Link from 'next/link'
-import { RiDashboard2Fill } from 'react-icons/ri'
+import Sidenav from './Sidenav'
 
-import { navLinks } from '@/data/data'
-
-const CommonSidebar = () => {
-  return (
-    <div className="h-screen overflow-y-auto">
-      <div aria-label="sidebar" className="py-8 px-3">
-        <Link href="/">
-          <a className="sidebar_link">
-            <RiDashboard2Fill
-              className="text-3xl text-purple-700"
-              aria-hidden="true"
-            />{' '}
-            <p className="text-lg lg:text-xl">eStore</p>
-          </a>
-        </Link>
-        <hr aria-hidden="true" className="mt-2 border-gray-700" />
-        <nav>
-          {navLinks.map((item) => (
-            <div key={item.title} className="space-y-3 pt-2">
-              <p className="mt-4 text-xl font-light uppercase xl:text-2xl">
-                {item.title}
-              </p>
-              <ul className="space-y-2">
-                {item.links.map((link) => (
-                  <li key={link.name} className="ml-4">
-                    <Link
-                      href={`${
-                        link.name.toLowerCase() === 'overview'
-                          ? '/'
-                          : link.name.toLowerCase()
-                      }`}
-                    >
-                      <a className="sidebar_link">
-                        <link.Icon aria-hidden="true" className="text-2xl" />
-                        <p className="text-sm font-medium capitalize md:text-base">
-                          {link.name}
-                        </p>
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </nav>
-      </div>
-    </div>
-  )
-}
-const SidebarLg = () => {
-  return (
-    <div className="sticky top-0 hidden h-screen w-60 bg-slate-900 lg:block">
-      <CommonSidebar />
-    </div>
-  )
-}
 const Sidebar = () => {
   return (
     <div className="relative z-50 border-r border-gray-700">
-      <SidebarLg />
+      <div className="sticky top-0 hidden h-screen w-60 bg-slate-900 lg:block">
+        <Sidenav />
+      </div>
     </div>
   )
 }
